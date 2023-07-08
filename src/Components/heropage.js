6d220e5be79cc2fd50d168eda83f03ff63a2efb0
rs-kannan <rskannansrinivasan@gmail.com>
2023-07-08 17:19:01 +0530

    first commit

--- src/Components/heropage.js
+++ src/Components/heropage.js
@@ -38,8 +38,8 @@ return(
         <header class="bg-dark py-5">
             <div class="container px-4 px-lg-5 my-5">
                 <div class="text-center text-white">
-                    <h1 class="display-4 fw-bolder">CROMA</h1>
-                    <p class="lead fw-normal text-white-50 mb-0">Designed By Gowtham</p>
+                    <h1 class="display-4 fw-bolder b">WALMART</h1>
+                    <p class="lead fw-normal text-white-50 mb-0"></p>
                 </div>
             </div>
         </header>
